235e3f4d1f0e15bce65ef0c29ecc2093f109f568
koszulordie <ferran.muinos@irbbarcelona.org>
2024-07-03 15:41:24 +0200

    MINOR UPDATE related to running config

--- cleaner.sh
+++ cleaner.sh
@@ -2,15 +2,13 @@
 
 rm -r work/
 rm -r .nextflow/
-rm timeline.html*
-rm trace.txt*
+rm timeline-*
+rm trace-*
 rm .nextflow.log*
 
 find . -maxdepth 4 -type d -name .idea -exec rm -r {} +
-# rm -r .idea/
 
 find . -maxdepth 4 -type d -name __pycache__ -exec rm -r {} +
-#rm -r __pycache__/
 
 
 
