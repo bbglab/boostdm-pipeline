87ae750fb0f4a490dcf0783b2c1845ad304d86a5
koszulordie <ferran.muinos@irbbarcelona.org>
2024-07-10 14:48:16 +0200

    MODIFY scan_errors.sh script to fit the new nextflow version in conda

--- scan_errors.sh
+++ scan_errors.sh
@@ -1,3 +1,3 @@
 # uses conda environment boostdm-new-pipeline
 
-cat trace-20240626-38172655.txt | grep FAILED | cut -f3 | python _scan_errors.py
+cat $1 | grep FAILED | cut -f3 | python _scan_errors.py
